b9526a46a9a3aecec1fbc6a54ba2dbeeb3b1aa87
matthieu <matthieu@openbsd.org>
2007-02-04 20:20:19 +0000

    Clean up sockets after Xorg -configure.

--- xserver/hw/xfree86/common/xf86Configure.c
+++ xserver/hw/xfree86/common/xf86Configure.c
@@ -1030,6 +1030,7 @@ DoConfigure()
     ErrorF("To test the server, run 'X -config %s'\n\n", filename);
 
 bail:
+    CloseWellKnownConnections();
     OsCleanup(TRUE);
     AbortDDX();
     fflush(stderr);
