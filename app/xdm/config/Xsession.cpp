8eda1a96a2fce4eedb4bd54851e77e1dc3fe7cc7
david <david@openbsd.org>
2007-04-04 22:29:12 +0000

    do_exit must be called before exiting so ssh-agent can be killed matches XF4 version; ok matthieu

--- app/xdm/config/Xsession.cpp
+++ app/xdm/config/Xsession.cpp
@@ -1,7 +1,7 @@
 XCOMM!SHELL_CMD
 XCOMM
 XCOMM $Xorg: Xsession,v 1.4 2000/08/17 19:54:17 cpqbld Exp $
-XCOMM $OpenBSD: Xsession.cpp,v 1.2 2006/11/28 15:41:12 matthieu Exp $
+XCOMM $OpenBSD: Xsession.cpp,v 1.3 2007/04/04 22:29:12 david Exp $
 XCOMM $XFree86: xc/programs/xdm/config/Xsession,v 1.2 1998/01/11 03:48:32 dawes Exp $
 
 XCOMM redirect errors to a file in user's home directory if we can
@@ -83,3 +83,4 @@ else
 	BINDIR/xterm &
 	BINDIR/fvwm
 fi
+do_exit
