a462491a0575efe1cdc0cbb3b4ef2b21f4434f39
matthieu <matthieu@openbsd.org>
2007-08-05 20:01:48 +0000

    Fix quoting for cpp processing

--- app/xdm/config/Xreset.cpp
+++ app/xdm/config/Xreset.cpp
@@ -2,4 +2,4 @@ XCOMM!/bin/sh
 XCOMM Deregister a login. (Derived from TakeConsole as follows:)
 XCOMM
 BINDIR/sessreg -d -w "/var/log/wtmp" -u "/var/run/utmp" \
-	-x "XDMCONFIGDIR/Xservers" -l $DISPLAY -h "" $USER
+	-x XDMCONFIGDIR/Xservers -l $DISPLAY -h "" $USER
