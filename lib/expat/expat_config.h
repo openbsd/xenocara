a8a2c415420042b8aefaa681cc6f357be1124c64
matthieu <matthieu@openbsd.org>
2007-10-20 12:49:39 +0000

    expat is now in base, xmlwf in ports

--- lib/expat/expat_config.h
+++
@@ -1,8 +0,0 @@
-/* $OpenBSD: expat_config.h,v 1.1 2006/11/26 12:03:55 matthieu Exp $ */
-
-/* quick and dirty conf for OpenBSD */
-
-#define HAVE_MEMMOVE 1
-#define XML_CONTEXT_BYTES 1024
-#define XML_DTD 1
-#define XML_NS 1
