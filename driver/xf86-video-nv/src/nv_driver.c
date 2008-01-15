856e0243ab6eaa6a450651b9877042f734422fb1
matthieu <matthieu@openbsd.org>
2008-01-15 07:37:23 +0000

    Support GeForce 8800 GT. From X.Org git repository.

--- driver/xf86-video-nv/src/nv_driver.c
+++ driver/xf86-video-nv/src/nv_driver.c
@@ -383,6 +383,7 @@ static SymTabRec NVKnownChipsets[] =
   { 0x10DE042B, "Quadro NVS 135M" },
   { 0x10DE042D, "Quadro FX 360M" },
   { 0x10DE042F, "Quadro NVS 290" },
+  { 0x10DE0611, "GeForce 8800 GT" },
 
   {-1, NULL}
 };
@@ -748,6 +749,7 @@ NVIsG80(int chipType)
         case 0x0190:
         case 0x0400:
         case 0x0420:
+        case 0x0610:
             return TRUE;
     }
 
