37eae9b7cfb9e2dadb2926ee0e69f282c618ce80
matthieu <matthieu@openbsd.org>
2008-01-27 14:03:40 +0000

    Fix m68k builds. Similar to the fix that miod used.

--- xserver/hw/xfree86/os-support/bsd/hp300_video.c
+++ xserver/hw/xfree86/os-support/bsd/hp300_video.c
@@ -1,5 +1,4 @@
-/* $XFree86$ */
-/* $OpenBSD: hp300_video.c,v 1.1 2006/11/28 20:29:31 matthieu Exp $	*/
+/* $OpenBSD: hp300_video.c,v 1.2 2008/01/27 14:03:40 matthieu Exp $	*/
 /*
  * Copyright 1992 by Rich Murphey <Rich@Rice.edu>
  * Copyright 1993 by David Wexelblat <dwex@goblin.org>
@@ -8,26 +7,28 @@
  * documentation for any purpose is hereby granted without fee, provided that
  * the above copyright notice appear in all copies and that both that
  * copyright notice and this permission notice appear in supporting
- * documentation, and that the names of Rich Murphey and David Wexelblat 
- * not be used in advertising or publicity pertaining to distribution of 
+ * documentation, and that the names of Rich Murphey and David Wexelblat
+ * not be used in advertising or publicity pertaining to distribution of
  * the software without specific, written prior permission.  Rich Murphey and
- * David Wexelblat make no representations about the suitability of this 
- * software for any purpose.  It is provided "as is" without express or 
+ * David Wexelblat make no representations about the suitability of this
+ * software for any purpose.  It is provided "as is" without express or
  * implied warranty.
  *
- * RICH MURPHEY AND DAVID WEXELBLAT DISCLAIM ALL WARRANTIES WITH REGARD TO 
- * THIS SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND 
- * FITNESS, IN NO EVENT SHALL RICH MURPHEY OR DAVID WEXELBLAT BE LIABLE FOR 
- * ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER 
- * RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF 
- * CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN 
+ * RICH MURPHEY AND DAVID WEXELBLAT DISCLAIM ALL WARRANTIES WITH REGARD TO
+ * THIS SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS, IN NO EVENT SHALL RICH MURPHEY OR DAVID WEXELBLAT BE LIABLE FOR
+ * ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER
+ * RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF
+ * CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
  * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  *
  */
 
-/* $XConsortium: bsd_video.c /main/10 1996/10/25 11:37:57 kaleb $ */
+#ifdef HAVE_XORG_CONFIG_H
+#include <xorg-config.h>
+#endif
 
-#include "X.h"
+#include <X11/X.h>
 #include "xf86.h"
 #include "xf86Priv.h"
 
@@ -65,26 +66,27 @@ hp300MapVidMem(int ScreenNum, unsigned long Base, unsigned long Size, int flags)
 	pointer base;
 
 #ifdef DEBUG
-	ErrorF("mapVidMem %lx, %lx, fd = %d\n", Base, Size, fd);
+	xf86MsgVerb(X_INFO, 3, "mapVidMem %lx, %lx, fd = %d\n",
+	    Base, Size, fd);
 #endif
 
-	base = mmap(0, Size, PROT_READ|PROT_WRITE, MAP_SHARED, fd, Base);
+       base = mmap(0, Size,
+                   (flags & VIDMEM_READONLY) ?
+                    PROT_READ : (PROT_READ | PROT_WRITE),
+                   MAP_SHARED, fd, Base);
 	if (base == MAP_FAILED)
 		FatalError("%s: could not mmap screen [s=%x,a=%x] (%s)",
 			   "xf86MapVidMem", Size, Base, strerror(errno));
-
 	return base;
 }
-
 
 static void
 hp300UnmapVidMem(int ScreenNum, pointer Base, unsigned long Size)
 {
-
 	munmap(Base, Size);
 }
 
-int
+_X_EXPORT int
 xf86ReadBIOS(unsigned long Base, unsigned long Offset, unsigned char *Buf,
 	     int Len)
 {
@@ -95,26 +97,27 @@ xf86ReadBIOS(unsigned long Base, unsigned long Offset, unsigned char *Buf,
 /* Interrupt Handling section                                              */
 /***************************************************************************/
 
-Bool
+_X_EXPORT Bool
 xf86DisableInterrupts()
 {
 
 	return(TRUE);
 }
 
-void
+_X_EXPORT void
 xf86EnableInterrupts()
 {
 
 	return;
 }
 
+#ifdef X_PRIVSEP
 /*
- * Do all initialisation that need root privileges 
+ * Do all initialisation that need root privileges
  */
-void
+_X_EXPORT void
 xf86PrivilegedInit(void)
 {
-	/* pciInit(); */
 	xf86OpenConsole();
 }
+#endif
