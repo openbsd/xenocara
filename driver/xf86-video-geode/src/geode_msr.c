e37176280ecbb0ae6caf545554dfbdaf3cb245c0
mbalmer <mbalmer@openbsd.org>
2008-06-14 22:26:13 +0000

    Changes needed to run on OpenBSD using the new amdmsr(4) driver.

--- driver/xf86-video-geode/src/geode_msr.c
+++ driver/xf86-video-geode/src/geode_msr.c
@@ -1,11 +1,25 @@
 #define _LARGEFILE64_SOURCE
+
+#include <sys/types.h>
+#include <sys/ioctl.h>
+#ifdef __OpenBSD__
+#include <sys/amdmsr.h>
+#endif
+
+#include <errno.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <unistd.h>
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/errno.h>
+
 #include "os.h"
 #include "geode.h"
+
+#ifdef __OpenBSD__
+#define _PATH_MSRDEV	"/dev/amdmsr"
+#define X_PRIVSEP
+#else
+#define _PATH_MSRDEV	"/dev/cpu/0/msr"
+#endif
 
 static int
 _msr_open(void)
@@ -13,9 +27,13 @@ _msr_open(void)
     static int msrfd = 0;
 
     if (msrfd == 0) {
-	msrfd = open("/dev/cpu/0/msr", O_RDWR);
+#ifdef X_PRIVSEP
+	msrfd = priv_open_device(_PATH_MSRDEV);
+#else
+	msrfd = open(_PATH_MSRDEV, O_RDWR);
+#endif
 	if (msrfd == -1)
-	    ErrorF("Unable to open /dev/cpu/0/msr: %d\n", errno);
+	    ErrorF("Unable to open %s: %s\n", _PATH_MSRDEV, strerror(errno));
     }
 
     return msrfd;
@@ -24,6 +42,20 @@ _msr_open(void)
 int
 GeodeReadMSR(unsigned long addr, unsigned long *lo, unsigned long *hi)
 {
+#ifdef __OpenBSD__
+    struct amdmsr_req req;
+#endif
+    int fd = _msr_open();
+
+#ifdef __OpenBSD__
+    req.addr = addr;
+
+    if (ioctl(fd, RDMSR, &req) == -1)
+	ErrorF("Unable to RDMSR %d\n", errno);
+
+    *hi = req.val >> 32;
+    *lo = req.val & 0xffffffff;
+#else
     unsigned int data[2];
     int fd = _msr_open();
     int ret;
@@ -43,13 +75,25 @@ GeodeReadMSR(unsigned long addr, unsigned long *lo, unsigned long *hi)
 
     *hi = data[1];
     *lo = data[0];
-
+#endif
     return 0;
 }
 
 int
 GeodeWriteMSR(unsigned long addr, unsigned long lo, unsigned long hi)
 {
+#ifdef __OpenBSD__
+    struct amdmsr_req req;
+#endif
+    int fd = _msr_open();
+
+#ifdef __OpenBSD__
+    req.addr = addr;
+    req.val = (u_int64_t)hi << 32 | (u_int64_t)lo;
+
+    if (ioctl(fd, WRMSR, &req) == -1)
+	ErrorF("Unable to WRMSR %d\n", errno);
+#else
     unsigned int data[2];
     int fd = _msr_open();
 
@@ -64,6 +108,6 @@ GeodeWriteMSR(unsigned long addr, unsigned long lo, unsigned long hi)
 
     if (write(fd, (void *)data, 8) != 8)
 	return -1;
-
+#endif
     return 0;
 }
