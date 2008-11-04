65e3b816dc689194168c99e806b85d35fb50bdfe
todd <todd@openbsd.org>
2008-11-04 16:50:50 +0000

    variable declarations at the beginning of the scope; found by sparc (a gcc2 arch)

--- xserver/hw/xfree86/common/xf86Init.c
+++ xserver/hw/xfree86/common/xf86Init.c
@@ -484,6 +484,7 @@ InitOutput(ScreenInfo *pScreenInfo, int argc, char **argv)
   xf86Initialising = TRUE;
 
   if (serverGeneration == 1) {
+    GDevPtr configured_device;
 
     pScreenInfo->numScreens = 0;
 
@@ -563,7 +564,7 @@ InitOutput(ScreenInfo *pScreenInfo, int argc, char **argv)
     /* If there aren't any specified in the config file, autoconfig them */
     /* FIXME: Does not handle multiple active screen sections, but I'm not
      * sure if we really want to handle that case*/
-    GDevPtr configured_device = xf86ConfigLayout.screens->screen->device;
+    configured_device = xf86ConfigLayout.screens->screen->device;
     if ((!configured_device) || (!configured_device->driver)) {
         if (!autoConfigDevice(configured_device)) {
             xf86Msg(X_ERROR, "Automatic driver configuration failed\n");
