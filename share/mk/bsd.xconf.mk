582984b338115897a5da4c201c238ce85940afa6
matthieu <matthieu@openbsd.org>
2009-06-04 01:12:44 +0000

    simplify.

--- share/mk/bsd.xconf.mk
+++ share/mk/bsd.xconf.mk
@@ -1,20 +1,15 @@
-# $OpenBSD: bsd.xconf.mk,v 1.9 2009/05/22 17:58:45 matthieu Exp $
+# $OpenBSD: bsd.xconf.mk,v 1.10 2009/06/04 01:12:44 matthieu Exp $
 
 # Shared libs?
-.if ${MACHINE} == "mvme88k" || ${MACHINE} == "vax"
+.if ${MACHINE_ARCH} == "m88k" || ${MACHINE} == "vax"
 XENOCARA_HAVE_SHARED_LIBS?=no
 .else
 XENOCARA_HAVE_SHARED_LIBS?=yes
 .endif
 
-# Build GL libs and apps
-.if ${MACHINE_ARCH} == "m88k" || ${MACHINE} == vax
-XENOCARA_BUILD_GL?=no
-XENOCARA_BUILD_DRI?=no
-.else
-XENOCARA_BUILD_GL?=yes
-XENOCARA_BUILD_DRI?=yes
-.endif
+# Build GL libs and apps?
+XENOCARA_BUILD_GL?=${XENOCARA_HAVE_SHARED_LIBS:L}
+XENOCARA_BUILD_DRI?=${XENOCARA_HAVE_SHARED_LIBS:L}
 
 # Build XCB
 XENOCARA_BUILD_XCB?=no
