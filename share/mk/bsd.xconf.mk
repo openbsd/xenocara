7edc2b5a56ebd52b927d97a149daea76a14461e5
matthieu <matthieu@openbsd.org>
2009-08-04 15:45:41 +0000

    Enable XCB on architectures with shared libs.
    requested by sthen@.

--- share/mk/bsd.xconf.mk
+++ share/mk/bsd.xconf.mk
@@ -1,4 +1,4 @@
-# $OpenBSD: bsd.xconf.mk,v 1.10 2009/06/04 01:12:44 matthieu Exp $
+# $OpenBSD: bsd.xconf.mk,v 1.11 2009/08/04 15:45:41 matthieu Exp $
 
 # Shared libs?
 .if ${MACHINE_ARCH} == "m88k" || ${MACHINE} == "vax"
@@ -12,4 +12,4 @@ XENOCARA_BUILD_GL?=${XENOCARA_HAVE_SHARED_LIBS:L}
 XENOCARA_BUILD_DRI?=${XENOCARA_HAVE_SHARED_LIBS:L}
 
 # Build XCB
-XENOCARA_BUILD_XCB?=no
+XENOCARA_BUILD_XCB?=${XENOCARA_HAVE_SHARED_LIBS}
