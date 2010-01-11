09d912a54be7166be0c1681c7e3a8e789c59cf01
matthieu <matthieu@openbsd.org>
2010-01-11 20:29:48 +0000

    remove xtrap applications. They are disconnected from build for weeks now.

--- app/xtrap/XEKeybCtrl.h
+++
@@ -1,8 +0,0 @@
-/* $XFree86$ */
-
-extern int XEEnableCtrlKeys(void(*rtn)(int));
-extern int XEEnableCtrlC(void (*rtn)(int));
-extern int XEEnableCtrlY(void (*rtn)(int));
-extern int XEClearCtrlKeys(void);
-extern int XEDeclExitHndlr(void (*rtn)(int));
-
