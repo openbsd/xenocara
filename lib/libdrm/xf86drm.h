e4c82170c3ad12caa8acc794aea3f182f856d8f8
oga <oga@openbsd.org>
2010-07-18 21:02:35 +0000

    Support the apis for getting vblank events from the drm fd. (only vblank events for now, pageflip events require kms and thus have not yet been added).
    Bumps libdrm minor version.

--- lib/libdrm/xf86drm.h
+++ lib/libdrm/xf86drm.h
@@ -669,6 +669,23 @@ extern void drmMsg(const char *format, ...);
 extern int drmSetMaster(int fd);
 extern int drmDropMaster(int fd);
 
+#define DRM_EVENT_CONTEXT_VERSION 1
+
+typedef struct _drmEventContext {
+
+	/* This struct is versioned so we can add more pointers if we
+	 * add more events. */
+	int version;
+
+	void (*vblank_handler)(int fd,
+			       unsigned int sequence, 
+			       unsigned int tv_sec,
+			       unsigned int tv_usec,
+			       void *user_data);
+} drmEventContext, *drmEventContextPtr;
+
+extern int drmHandleEvent(int fd, drmEventContextPtr evctx);
+
 extern char *drmGetDeviceNameFromFd(int fd);
 
 #endif
