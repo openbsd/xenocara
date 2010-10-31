d5b8b84f35657a90d30d1fa601dbd4e30dbb33b6
matthieu <matthieu@openbsd.org>
2010-10-31 19:05:48 +0000

    Update to appres 1.0.3. No functional change.

--- app/appres/appres.c
+++ app/appres/appres.c
@@ -1,6 +1,4 @@
 /*
- * $Xorg: appres.c,v 1.4 2001/02/09 02:05:27 xorgcvs Exp $
- *
  * 
 Copyright 1989, 1998  The Open Group
 
@@ -26,8 +24,6 @@ in this Software without prior written authorization from The Open Group.
  * *
  * Author:  Jim Fulton, MIT X Consortium
  */
-
-/* $XFree86: xc/programs/appres/appres.c,v 1.3 2001/01/17 23:44:49 dawes Exp $ */
 
 #include <X11/Intrinsic.h>
 #include <stdio.h>
