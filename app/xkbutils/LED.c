499d07868c3c1a3a82d836b3222cfb4ff116c641
matthieu <matthieu@openbsd.org>
2010-10-31 19:49:20 +0000

    Update to xkbutils 1.0.3. No functional change.

--- app/xkbutils/LED.c
+++ app/xkbutils/LED.c
@@ -31,8 +31,6 @@
 #include <X11/Xos.h>
 #include <X11/Xaw/XawInit.h>
 #include "LEDP.h"
-#include <X11/Xmu/Converters.h>
-#include <X11/Xmu/Drawing.h>
 #include <stdio.h>
 #include <ctype.h>
 /* needed for abs() */
