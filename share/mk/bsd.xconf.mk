07dcad530f3305993538b4f3c4ebae6bdd44b177
matthieu <matthieu@openbsd.org>
2010-11-27 14:20:47 +0000

    pixman build fails on vax.

--- share/mk/bsd.xconf.mk
+++ share/mk/bsd.xconf.mk
@@ -1,4 +1,4 @@
-# $OpenBSD: bsd.xconf.mk,v 1.15 2010/11/25 21:47:19 matthieu Exp $
+# $OpenBSD: bsd.xconf.mk,v 1.16 2010/11/27 14:20:47 matthieu Exp $
 
 # Shared libs?
 .if ${MACHINE_ARCH} == "m88k" || ${MACHINE} == "vax"
@@ -15,4 +15,8 @@ XENOCARA_BUILD_DRI?=${XENOCARA_HAVE_SHARED_LIBS:L}
 XENOCARA_BUILD_XCB?=yes
 
 # Build pixman?
-XENOCARA_BUILD_PIXMAN?=${XENOCARA_BUILD_XCB}
+.if ${MACHINE} == "vax"
+XENOCARA_BUILD_PIXMAN?=no
+.else
+XENOCARA_BUILD_PIXMAN?=yes
+.endif
