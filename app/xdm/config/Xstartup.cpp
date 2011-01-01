831b8e77d09586bd025dbf5836ae7b54a6ba6f50
matthieu <matthieu@openbsd.org>
2011-01-01 20:39:44 +0000

    Fix duplicated code resulting from a bad merge of xdm 1.1.9.

--- app/xdm/config/Xstartup.cpp
+++ app/xdm/config/Xstartup.cpp
@@ -1,12 +1,10 @@
 XCOMM!/bin/sh
 XCOMM 
-XCOMM $OpenBSD: Xstartup.cpp,v 1.3 2010/03/28 09:33:02 matthieu Exp $
+XCOMM $OpenBSD: Xstartup.cpp,v 1.4 2011/01/01 20:39:44 matthieu Exp $
 XCOMM
-BINDIR/sessreg  -a -w WTMP_FILE -u UTMP_FILE \
-	-x XDMCONFIGDIR/Xservers -l $DISPLAY -h "" $USER
 if [ -f /etc/nologin ]; then
         /usr/X11R6/bin/xmessage -file /etc/nologin -timeout 30 -center
         exit 1
 fi
-BINDIR/sessreg  -a -w "/var/log/wtmp" -u "/var/run/utmp" \
+BINDIR/sessreg  -a -w WTMP_FILE -u UTMP_FILE \
 	-x XDMCONFIGDIR/Xservers -l $DISPLAY -h "" $USER
