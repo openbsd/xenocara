7ae39052dff21af6482fb7617a83e1be17306d16
matthieu <matthieu@openbsd.org>
2011-03-08 20:48:59 +0000

    xcb is no longer optional.

--- share/mk/bsd.xconf.mk
+++ share/mk/bsd.xconf.mk
@@ -1,4 +1,4 @@
-# $OpenBSD: bsd.xconf.mk,v 1.17 2011/03/08 07:59:23 matthieu Exp $
+# $OpenBSD: bsd.xconf.mk,v 1.18 2011/03/08 20:48:59 matthieu Exp $
 
 # Shared libs?
 .if ${MACHINE_ARCH} == "m88k" || ${MACHINE} == "vax"
@@ -10,9 +10,6 @@ XENOCARA_HAVE_SHARED_LIBS?=yes
 # Build GL libs and apps?
 XENOCARA_BUILD_GL?=${XENOCARA_HAVE_SHARED_LIBS:L}
 XENOCARA_BUILD_DRI?=${XENOCARA_HAVE_SHARED_LIBS:L}
-
-# Build XCB?
-XENOCARA_BUILD_XCB?=yes
 
 # Build pixman?
 .if ${MACHINE} == "vax"
