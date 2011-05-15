3d9ed32226679114eb130e53e76115c6560821ca
okan <okan@openbsd.org>
2011-05-15 17:58:47 +0000

    fix botched client and group cycle defines; noticed by Thomas Pfaff.
    "no cookie! fix it" - oga@

--- app/cwm/calmwm.h
+++ app/cwm/calmwm.h
@@ -15,7 +15,7 @@
  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  *
- * $OpenBSD: calmwm.h,v 1.125 2011/05/11 13:53:51 okan Exp $
+ * $OpenBSD: calmwm.h,v 1.126 2011/05/15 17:58:47 okan Exp $
  */
 
 #ifndef _CALMWM_H_
@@ -68,12 +68,12 @@
 #define	CWM_EXEC_WM		0x0002
 
 /* client cycle */
-#define CWM_CYCLE		0x0001
-#define CWM_RCYCLE		0x0002
+#define CWM_CYCLE		0
+#define CWM_RCYCLE		1
 
 /* group cycle */
-#define CWM_CYCLEGROUP		0x0001
-#define CWM_RCYCLEGROUP		0x0002
+#define CWM_CYCLEGROUP		0
+#define CWM_RCYCLEGROUP		1
 
 #define KBTOGROUP(X) ((X) - 1)
 
