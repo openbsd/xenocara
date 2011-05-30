89a8ad0171a63ea96ab3afa77a077d6f404a5020
shadchin <shadchin@openbsd.org>
2011-05-30 10:52:57 +0000

    If you do not have XkbOptions in xorg.conf, it is automatically set to "terminate: ctrl_alt_bksp" (only if XENOCARA_USE_XKEYBOARD_CONFIG=Yes)
    tested ajacoutot@, ok matthieu@

--- driver/xf86-input-keyboard/src/bsd_kbd.c
+++ driver/xf86-input-keyboard/src/bsd_kbd.c
@@ -57,7 +57,11 @@ struct nameint kbdvar[] = {
 };
 
 struct nameint kbdopt[] = {
+#ifndef USE_XKEYBOARD_CONFIG
 	{ KB_SWAPCTRLCAPS, "ctrl:swapcaps" },
+#else
+	{ KB_SWAPCTRLCAPS, "ctrl:swapcaps,terminate:ctrl_alt_bksp" },
+#endif
 	{ 0 }
 };
 #endif
