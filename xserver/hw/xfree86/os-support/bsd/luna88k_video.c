2db9dc4ecc877239cfcfc3fcba555b3cfa805d53
aoyama <aoyama@openbsd.org>
2013-07-16 14:39:22 +0000

    Re-commit luna88k_video.c.  Previous file is 'doubled' by mistake.

--- xserver/hw/xfree86/os-support/bsd/luna88k_video.c
+++ xserver/hw/xfree86/os-support/bsd/luna88k_video.c
@@ -1,4 +1,4 @@
-/* $OpenBSD: luna88k_video.c,v 1.1 2013/07/15 13:23:17 aoyama Exp $	*/
+/* $OpenBSD: luna88k_video.c,v 1.2 2013/07/16 14:39:22 aoyama Exp $	*/
 /*
  * Copyright 1992 by Rich Murphey <Rich@Rice.edu>
  * Copyright 1993 by David Wexelblat <dwex@goblin.org>
@@ -45,91 +45,6 @@ xf86OSInitVidMem(VidMemInfoPtr pVidMem)
 	pVidMem->linearSupported = TRUE;
 	pVidMem->mapMem = NULL;   /* luna88kMapVidMem; */
 	pVidMem->unmapMem = NULL; /* luna88kUnmapVidMem; */
-	pVidMem->initialised = TRUE;
-}
-
-_X_EXPORT int
-xf86ReadBIOS(unsigned long Base, unsigned long Offset, unsigned char *Buf,
-	     int Len)
-{
-	return 0;
-}
-
-/***************************************************************************/
-/* Interrupt Handling section                                              */
-/***************************************************************************/
-
-_X_EXPORT Bool
-xf86DisableInterrupts()
-{
-
-	return TRUE;
-}
-
-_X_EXPORT void
-xf86EnableInterrupts()
-{
-
-	return;
-}
-
-#ifdef X_PRIVSEP
-/*
- * Do all initialisation that need root privileges
- */
-_X_EXPORT void
-xf86PrivilegedInit(void)
-{
-	xf86OpenConsole();
-}
-#endif
-/* $OpenBSD: luna88k_video.c,v 1.1 2013/07/15 13:23:17 aoyama Exp $	*/
-/*
- * Copyright 1992 by Rich Murphey <Rich@Rice.edu>
- * Copyright 1993 by David Wexelblat <dwex@goblin.org>
- *
- * Permission to use, copy, modify, distribute, and sell this software and its
- * documentation for any purpose is hereby granted without fee, provided that
- * the above copyright notice appear in all copies and that both that
- * copyright notice and this permission notice appear in supporting
- * documentation, and that the names of Rich Murphey and David Wexelblat
- * not be used in advertising or publicity pertaining to distribution of
- * the software without specific, written prior permission.  Rich Murphey and
- * David Wexelblat make no representations about the suitability of this
- * software for any purpose.  It is provided "as is" without express or
- * implied warranty.
- *
- * RICH MURPHEY AND DAVID WEXELBLAT DISCLAIM ALL WARRANTIES WITH REGARD TO
- * THIS SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
- * FITNESS, IN NO EVENT SHALL RICH MURPHEY OR DAVID WEXELBLAT BE LIABLE FOR
- * ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER
- * RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF
- * CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
- * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
- *
- */
-
-#ifdef HAVE_XORG_CONFIG_H
-#include <xorg-config.h>
-#endif
-
-#include <X11/X.h>
-#include "xf86.h"
-#include "xf86Priv.h"
-
-#include "xf86_OSlib.h"
-#include "xf86OSpriv.h"
-
-/***************************************************************************/
-/* Video Memory Mapping section                                            */
-/***************************************************************************/
-
-void
-xf86OSInitVidMem(VidMemInfoPtr pVidMem)
-{
-	pVidMem->linearSupported = TRUE;
-	pVidMem->mapMem = NULL;
-	pVidMem->unmapMem = NULL;
 	pVidMem->initialised = TRUE;
 }
 
