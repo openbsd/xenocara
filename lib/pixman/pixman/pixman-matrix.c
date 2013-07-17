2673d4da6bfea933f14601a0b3038bc2af50f8f7
miod <miod@openbsd.org>
2013-07-17 19:27:22 +0000

    Do not blindly use __builtin_clz() #ifdef __GNUC__, but only if gcc >= 3.4 for it did not exist in previous releases. ok matthieu@

--- lib/pixman/pixman/pixman-matrix.c
+++ lib/pixman/pixman/pixman-matrix.c
@@ -37,7 +37,8 @@
 static force_inline int
 count_leading_zeros (uint32_t x)
 {
-#ifdef __GNUC__
+#if defined(__GNUC__) && \
+	(__GNUC__ > 3 || (__GNUC__ == 3 && __GNUC_MINOR__ >= 4))
     return __builtin_clz (x);
 #else
     int n = 0;
