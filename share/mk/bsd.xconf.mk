737b662b93482d5e5c704733aa60133969b30c27
miod <miod@openbsd.org>
2013-08-22 19:26:51 +0000

    Gallium requires gcc 4 __sync_and_fetch() support, so don't bother trying to build it on platforms which don't provide this. ok jsg@ matthieu@

--- share/mk/bsd.xconf.mk
+++ share/mk/bsd.xconf.mk
@@ -1,4 +1,4 @@
-# $OpenBSD: bsd.xconf.mk,v 1.26 2013/08/17 17:05:43 bcallah Exp $
+# $OpenBSD: bsd.xconf.mk,v 1.27 2013/08/22 19:26:51 miod Exp $
 
 # Shared libs?
 .if ${MACHINE} == "vax"
@@ -15,7 +15,15 @@ XENOCARA_BUILD_DRI?=no
 XENOCARA_BUILD_GL?=${XENOCARA_HAVE_SHARED_LIBS:L}
 XENOCARA_BUILD_DRI?=${XENOCARA_HAVE_SHARED_LIBS:L}
 .endif
+# Gallium requires gcc 4 __sync_and_fetch() support
+.if ${MACHINE_ARCH} == "arm" || ${MACHINE_ARCH} == "hppa" || \
+    ${MACHINE_ARCH} == "hppa64" || ${MACHINE_ARCH} == "m68k" || \
+    ${MACHINE_ARCH} == "m88k" || ${MACHINE_CPU} == "mips64" || \
+    ${MACHINE_ARCH} == "sh" || ${MACHINE_ARCH} == "sparc"
+XENOCARA_BUILD_GALLIUM?=no
+.else
 XENOCARA_BUILD_GALLIUM?=${XENOCARA_HAVE_SHARED_LIBS:L}
+.endif
 
 # Build pixman?
 .if ${MACHINE} == "vax"
