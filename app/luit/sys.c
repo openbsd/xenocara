bb0625052330a5eef095b5f10c6da1f6e21eb81f
naddy <naddy@openbsd.org>
2016-01-13 21:17:02 +0000

    On BSD, switching off ICANON does not disable LNEXT (^V) processing. We explicitly need to unset IEXTEN.  ok matthieu@

--- app/luit/sys.c
+++ app/luit/sys.c
@@ -256,7 +256,7 @@ setRawTermios(void)
     rc = tcgetattr(0, &tio);
     if (rc < 0)
 	return rc;
-    tio.c_lflag &= (unsigned) ~(ECHO | ICANON | ISIG);
+    tio.c_lflag &= (unsigned) ~(ECHO | ICANON | IEXTEN | ISIG);
     tio.c_iflag &= (unsigned) ~(ICRNL | IXOFF | IXON | ISTRIP);
 #ifdef ONLCR
     tio.c_oflag &= (unsigned) ~ONLCR;
