7a95b9000b2d8061d2e0b008d6ba398bfcb11f07
matthieu <matthieu@openbsd.org>
2016-08-06 09:51:36 +0000

    Update to libXfixes 5.0.2. No functional changes.

--- lib/libXfixes/src/Xfixesint.h
+++ lib/libXfixes/src/Xfixesint.h
@@ -60,18 +60,4 @@ XFixesFindDisplay (Display *dpy);
 #define XFixesSimpleCheckExtension(dpy,i) \
   if (!XFixesHasExtension(i)) { return; }
 
-#ifndef HAVE__XEATDATAWORDS
-#include <X11/Xmd.h>  /* for LONG64 on 64-bit platforms */
-#include <limits.h>
-
-static inline void _XEatDataWords(Display *dpy, unsigned long n)
-{
-# ifndef LONG64
-    if (n >= (ULONG_MAX >> 2))
-        _XIOError(dpy);
-# endif
-    _XEatData (dpy, n << 2);
-}
-#endif
-
 #endif /* _XFIXESINT_H_ */
