e1e9d732f339c855ae66212da1ec3c5d2f0f150e
matthieu <matthieu@openbsd.org>
2017-07-26 21:14:54 +0000

    Stop abusing cpp as generic macro processor in the build system.
    ok naddy@

--- app/xenodm/config/Xreset.cpp
+++
@@ -1,5 +0,0 @@
-XCOMM!/bin/sh
-XCOMM Deregister a login. (Derived from TakeConsole as follows:)
-XCOMM
-BINDIR/sessreg -d -w WTMP_FILE -u UTMP_FILE \
-	-x XENODMCONFIGDIR/Xservers -l $DISPLAY -h "" $USER
