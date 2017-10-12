ca5563fecaf7f6b11d2aa4fcbd42d9724c4782fc
matthieu <matthieu@openbsd.org>
2017-10-12 19:32:43 +0000

    Remove xdm. Unhooked since more than 6 months.

--- app/xdm/config/Xreset.cpp
+++
@@ -1,5 +0,0 @@
-XCOMM!/bin/sh
-XCOMM Deregister a login. (Derived from TakeConsole as follows:)
-XCOMM
-BINDIR/sessreg -d -w WTMP_FILE -u UTMP_FILE \
-	-x XDMCONFIGDIR/Xservers -l $DISPLAY -h "" $USER
