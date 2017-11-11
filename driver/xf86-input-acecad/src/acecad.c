663938b0b9323867ff567f12cfefaf303ef956f7
matthieu <matthieu@openbsd.org>
2017-11-11 17:12:40 +0000

    xf86-input-acecad: adapt to latest xserver input driver API.

--- driver/xf86-input-acecad/src/acecad.c
+++ driver/xf86-input-acecad/src/acecad.c
@@ -491,7 +491,7 @@ AceCadPreInit(InputDriverPtr drv, InputInfoPtr local, int flags)
 
     if (local->fd != -1)
     {
-        RemoveEnabledDevice (local->fd);
+        xf86RemoveEnabledDevice (local);
         if (priv->buffer)
         {
             XisbFree(priv->buffer);
@@ -499,7 +499,7 @@ AceCadPreInit(InputDriverPtr drv, InputInfoPtr local, int flags)
         }
         xf86CloseSerial(local->fd);
     }
-    RemoveEnabledDevice (local->fd);
+    xf86RemoveEnabledDevice (local);
     local->fd = -1;
     return Success;
 
@@ -601,7 +601,7 @@ DeviceOff (DeviceIntPtr dev)
 
     if (local->fd != -1)
     {
-        RemoveEnabledDevice (local->fd);
+        xf86RemoveEnabledDevice (local);
         if (priv->buffer)
         {
             XisbFree(priv->buffer);
