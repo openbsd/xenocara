4ae54e6e96857efc013aaff2d06194ad8a2300e1
matthieu <matthieu@openbsd.org>
2018-03-20 20:33:14 +0000

    Force radeon driver to EXA acceleration on all chipsets where it's supported. Avoids panics caused by glamor reported by several people. ok deraadt@

--- driver/xf86-video-ati/src/radeon_glamor.c
+++ driver/xf86-video-ati/src/radeon_glamor.c
@@ -86,7 +86,7 @@ radeon_glamor_pre_init(ScrnInfoPtr scrn)
 	s = xf86GetOptValString(info->Options, OPTION_ACCELMETHOD);
 	if (!s) {
 		if (xorgGetVersion() >= XORG_VERSION_NUMERIC(1,18,3,0,0)) {
-			if (info->ChipFamily < CHIP_FAMILY_R600)
+			if (info->ChipFamily < CHIP_FAMILY_TAHITI)
 				return FALSE;
 		} else {
 			if (info->ChipFamily < CHIP_FAMILY_TAHITI)
