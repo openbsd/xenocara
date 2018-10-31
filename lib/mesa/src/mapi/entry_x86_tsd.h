f19a6c0440feba472bcb3ab3fdfe0718e421d6e2
jsg <jsg@openbsd.org>
2018-10-31 23:26:16 +0000

    Make similiar changes to the i386 tsd dispatch assembly as libglvnd to avoid a text relocation fixing the build with lld.
    ok kettenis@ naddy@

--- lib/mesa/src/mapi/entry_x86_tsd.h
+++ lib/mesa/src/mapi/entry_x86_tsd.h
@@ -31,7 +31,7 @@
 #define HIDDEN
 #endif
 
-#define X86_ENTRY_SIZE 32
+#define X86_ENTRY_SIZE 64
 
 __asm__(".text\n"
         ".balign 32\n"
@@ -44,12 +44,16 @@ __asm__(".text\n"
    func ":"
 
 #define STUB_ASM_CODE(slot)         \
-   "movl " ENTRY_CURRENT_TABLE ", %eax\n\t" \
-   "testl %eax, %eax\n\t"           \
-   "je 1f\n\t"                      \
-   "jmp *(4 * " slot ")(%eax)\n"    \
+   "call 1f\n\t"                    \
    "1:\n\t"                         \
-   "call " ENTRY_CURRENT_TABLE_GET "\n\t" \
+   "popl %eax\n\t"                  \
+   "addl $_GLOBAL_OFFSET_TABLE_+[.-1b], %eax\n\t" \
+   "movl " ENTRY_CURRENT_TABLE "@GOT(%eax), %eax\n\t" \
+   "mov (%eax), %eax\n\t"           \
+   "testl %eax, %eax\n\t"           \
+   "jne 1f\n\t"                     \
+   "call " ENTRY_CURRENT_TABLE_GET "@PLT\n\t" \
+   "1:\n\t"                         \
    "jmp *(4 * " slot ")(%eax)"
 
 #define MAPI_TMP_STUB_ASM_GCC
