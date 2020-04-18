806accb3da7c865b3b285bca94ee68bffd8ebc3d
matthieu <matthieu@openbsd.org>
2020-04-18 09:41:18 +0000

    Remove unused files.

--- xserver/hw/xfree86/dummylib/dispatchException.c
+++
@@ -1,8 +0,0 @@
-/**
- ** Public domain
- **/
-/* $OpenBSD: dispatchException.c,v 1.1 2006/11/28 20:29:31 matthieu Exp $ */
-
-#include "opaque.h"
-
-volatile char dispatchException;
--- xserver/hw/xfree86/dummylib/serverGeneration.c
+++
@@ -1,8 +0,0 @@
-/**
- ** Public domain
- **/
-/* $OpenBSD: serverGeneration.c,v 1.1 2006/11/28 20:29:31 matthieu Exp $ */
-
-#include "misc.h"
-
-unsigned long serverGeneration;
