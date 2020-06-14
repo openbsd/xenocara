60964e1bb63a9d3b72e00f5c9b8b63ecd4b38cb8
matthieu <matthieu@openbsd.org>
2020-06-14 16:02:38 +0000

    sync white space with upstream. No code change.

--- xserver/os/utils.c
+++ xserver/os/utils.c
@@ -920,7 +920,7 @@ ProcessCommandLine(int argc, char *argv[])
             defaultKeyboardControl.autoRepeat = TRUE;
         else if (strcmp(argv[i], "-r") == 0)
             defaultKeyboardControl.autoRepeat = FALSE;
-	else if ( strcmp( argv[i], "-retro") == 0)
+        else if (strcmp(argv[i], "-retro") == 0)
             party_like_its_1989 = TRUE;
         else if (strcmp(argv[i], "-s") == 0) {
             if (++i < argc)
