cb3c6b9276cfcdc588360d3a426acbe8913d9480
jsg <jsg@openbsd.org>
2020-06-18 11:45:13 +0000

    llvmpipe: use LLVM version string instead of re-computing it
    From Eric Engestrom
    7f5ef97a07d4054efb96f0d644344644023af82c in Mesa git
    
    fixes llvmpipe renderer string showing "LLVM 16.0" with LLVM 10

--- lib/mesa/src/gallium/drivers/llvmpipe/lp_screen.c
+++ lib/mesa/src/gallium/drivers/llvmpipe/lp_screen.c
@@ -97,8 +97,7 @@ static const char *
 llvmpipe_get_name(struct pipe_screen *screen)
 {
    static char buf[100];
-   snprintf(buf, sizeof(buf), "llvmpipe (LLVM %u.%u, %u bits)",
-            HAVE_LLVM >> 8, HAVE_LLVM & 0xff,
+   snprintf(buf, sizeof(buf), "llvmpipe (LLVM " MESA_LLVM_VERSION_STRING ", %u bits)",
             lp_native_vector_width );
    return buf;
 }
