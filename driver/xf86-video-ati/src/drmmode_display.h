9c091fab51b84f57886470506e6df79c6507674c
jsg <jsg@openbsd.org>
2021-01-18 01:33:50 +0000

    Fix link failure with gcc 10
    From Adam Jackson
    f223035f4ffcff2a9296d1e907a5193f8e8845a3 in xf86-video-ati git
    
    fixes clang 11 build
    mortimer@ had the same diff

--- driver/xf86-video-ati/src/drmmode_display.h
+++ driver/xf86-video-ati/src/drmmode_display.h
@@ -262,7 +262,7 @@ Bool drmmode_wait_vblank(xf86CrtcPtr crtc, drmVBlankSeqType type,
 			 uint64_t *ust, uint32_t *result_seq);
 
 
-miPointerSpriteFuncRec drmmode_sprite_funcs;
+extern miPointerSpriteFuncRec drmmode_sprite_funcs;
 
 
 #endif
