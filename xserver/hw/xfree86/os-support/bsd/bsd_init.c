cbb2480f27e868230a5dbe213b630ff8be01888c
matthieu <matthieu@openbsd.org>
2021-06-30 08:50:48 +0000

    Close the console fd after probing if it's a wscons, even it fails.
    This avoids keeping an open file descriptor on machines
    where /dev/console is not a wsdisplay device.

--- xserver/hw/xfree86/os-support/bsd/bsd_init.c
+++ xserver/hw/xfree86/os-support/bsd/bsd_init.c
@@ -627,9 +627,9 @@ xf86OpenWScons(void)
                 /* strip number, assuming 0 */
                 p = strchr(vtprefix, '0');
                 *p = '\0';
-	        close(fd);
 		fd = -1;
 	    }
+	    close(fd);
 	}
     }
     if (VTnum != -1) {
