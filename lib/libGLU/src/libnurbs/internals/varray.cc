6a599fd2aa0737800344519f6fd7b66792f1e75f
robert <robert@openbsd.org>
2023-09-05 15:35:17 +0000

    fix ISO C++17 does not allow 'register' storage class specifier error with clang16
    pulled from upstream: https://gitlab.freedesktop.org/mesa/glu/-/merge_requests/10
    
    ok jsg@

--- lib/libGLU/src/libnurbs/internals/varray.cc
+++ lib/libGLU/src/libnurbs/internals/varray.cc
@@ -73,8 +73,8 @@ Varray::~Varray( void )
 inline void
 Varray::update( Arc_ptr arc, long dir[2], REAL val )
 {
-    register long ds = sgn(arc->tail()[0] - arc->prev->tail()[0]);
-    register long dt = sgn(arc->tail()[1] - arc->prev->tail()[1]);
+    long ds = sgn(arc->tail()[0] - arc->prev->tail()[0]);
+    long dt = sgn(arc->tail()[1] - arc->prev->tail()[1]);
 
     if( dir[0] != ds || dir[1] != dt ) {
 	dir[0] = ds;
